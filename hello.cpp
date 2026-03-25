7b4e111a6bb00e3023a4de3ce9224968eeed0629
LyzetteJamias <jamiaslyzette@gmail.com>
2026-03-25 22:49:02 +0800

    take 2 whoops (ㆆ_ㆆ)

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int  main() {
     
+    cout << "Hello World!" ;
+    cout << "\nUsing the test branch";
+    
     cout << "Being afraid isn't a bad thing. It's my fear that brought me this far." ;
     cout << "\n- Eisen";
-    
+
+    cout << "(ㆆ_ㆆ)...";
+
 return 0;
 }
