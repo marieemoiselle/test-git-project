b8f596e366f15725ff45993efc963976ffb86101
maybrie <carubiofayeb.11@gmail.com>
2026-03-29 16:28:50 +0800

    add message

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int  main() {
     
     cout << "Hello World!" ;
+    cout << "\nWelcome to C++ programming";
     cout << "\nUsing the test branch";
     cout << "Done testing"
 return 0;
